72bee5732e95b2bf4c7c5d9fd63d5901f8b0e037
ahassaan071-commits <ahassaan071@gmail.com>
2026-09-07 05:53:27 +0000

    Sep 7, 2026, 10:53 AM

--- script.js
+++ script.js
@@ -27237,7 +27237,7 @@ if (teacherDashboard) {
     teacherDashboard.style.setProperty(
         "display",
         "flex",
-        // EduPortal
+        "important"
     );
 
     teacherDashboard.style.setProperty(
